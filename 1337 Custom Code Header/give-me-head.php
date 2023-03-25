f1e834ce0f5380eded4c974961c7ede8c7cb10a4
codelyfe <codelyfe@gmail.com>
2023-03-25 14:56:20 -0500

    Update give-me-head.php

--- 1337 Custom Code Header/give-me-head.php
+++ 1337 Custom Code Header/give-me-head.php
@@ -3,7 +3,7 @@
 Plugin Name: 1337 Custom Header Code Plugin
 Description: Allows users to add custom code to the head section of every page
 Version: 1337.0
-Author: Randal Burger Jr
+Author: ランダル・バーガー・ジュニア
 Author URI: https://codelyfe.github.io/
 License: GNU General Public License v2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
@@ -39,7 +39,7 @@ function custom_settings_page() {
     $custom_code = get_option( 'custom_code', '' );
     ?>
     <div class="wrap">
-        <h1 style="text-align:center;">Custom Code Settings<br/> by <a href="https://codelyfe.github.io" target="_blank">Codelyfe</a></h1>
+        <h1>Custom Code Settings</h1>
         <form method="post">
             <label for="header-code-editor">Custom Header Code:</label>
             <!-- Replace textarea with CodeMirror textarea -->
@@ -50,47 +50,14 @@ function custom_settings_page() {
     
     <!-- Add script tag to initialize CodeMirror on the textarea -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.63.1/codemirror.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.63.1/addon/mode/multiplex.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.63.1/mode/xml/xml.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.63.1/mode/css/css.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.63.1/mode/javascript/javascript.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.63.1/mode/htmlmixed/htmlmixed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.63.1/mode/javascript/javascript.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.63.1/codemirror.css">
     <script>
-        // Define the language modes to use
-        var modes = {
-            "htmlmixed": /^<!doctype html\b/i,
-            "xml": /^\s*<\?xml\b/,
-            "javascript": /^javascript\b/i,
-            "css": /^css\b/i,
-            "markdown": /^markdown\b/i,
-        };
-
-        // Create a regular expression to match any of the modes
-        var modeRegExp = new RegExp(Object.keys(modes).map(function(m) { return modes[m].source; }).join("|"));
-
-        // Create the CodeMirror editor with multiplexing enabled
-        var editor = CodeMirror.fromTextArea(document.getElementById("header-code-editor"), {
-            lineNumbers: true,
-            mode: {
-                name: "multiplex",
-                base: "text/html",
-                multiplex: function(mode, opts) {
-                    if (modeRegExp.test(mode)) {
-                        return CodeMirror.multiplexingMode(
-                            CodeMirror.getMode(opts, modes[mode]),
-                            {
-                                open: opts.openTag || "<" + mode + ">",
-                                close: opts.closeTag || "</" + mode + ">",
-                                modeProps: {hasAttributeMixed: true},
-                            }
-                        );
-                    }
-                    return null;
-                }
-            },
-            theme: "default",
-        });
+      var editor = CodeMirror.fromTextArea(document.getElementById("header-code-editor"), {
+        lineNumbers: true,
+        //mode: "javascript",
+        theme: "default",
+      });
     </script>
     <?php
 }
